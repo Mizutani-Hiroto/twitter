e74543e1686269dbce424e37a960f085e835570d
Mizutani-Hiroto <mztnhrt0428@gmail.com>
2020-10-15 01:31:13 +0000

    fix posts layouts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -16,8 +16,8 @@
         <button class="btn btn-outline-primary">Show</button>
         <a href="/posts/{{ $post->id }}/edit" class="btn btn-outline-primary">Edit</a>
         <form action="/posts/{{ $post->id }}" method="POST" onsubmit="if(confirm('Delete? Are you sure?')) { return true } else { return false };">
-          <input type="hidden" name"_method" value="DELETE">
-          <input type="hideen" name="_token" value="{{ csrf_token() }}">
+          <input type="hidden" name="_method" value="DELETE">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
           <button type="submit" class="btn btn-outline-danger">Delete</button>
         </form>
       </div>
